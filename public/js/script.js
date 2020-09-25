06804e9600a3b3f0942b79ab3df08241b57d2132
ImranCseKU <imran150240@gmail.com>
2020-09-26 02:13:38 +0600

    remove wrapper button jusing jQuery

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
       $('.nav-button').toggleClass('change');
   });
 
+  var width= $(window).width();
+  if(width < 425){
+      $(".find").on("focus", function(){
+          $('.wrapper').addClass('wrapper-change');
+      }).on("blur", function(){
+          $('.wrapper').removeClass('wrapper-change');
+      });
+  }
+
 });
 
 
