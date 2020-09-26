5c57670e95a0492e9b17814db43adc66871e38b2
ImranCseKU <imran150240@gmail.com>
2020-09-27 00:21:08 +0600

    remove wrapper for mobile device

--- public/js/script.js
+++ public/js/script.js
@@ -3,15 +3,6 @@ $(document).ready(function(){
       $('.nav-button').toggleClass('change');
   });
 
-  var width= $(window).width();
-  if(width < 425){
-      $(".find").on("focus", function(){
-          $('.wrapper').addClass('content-hide');
-      }).on("blur", function(){
-          $('.wrapper').removeClass('content-hide');
-      });
-  }
-
 });
 
 
