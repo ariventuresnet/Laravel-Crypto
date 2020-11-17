f41124703b378893e9ce63e16968f3e4d9acd3bd
ImranCseKU <imran150240@gmail.com>
2020-11-17 23:23:10 +0600

    add new layout

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 //Header Button
 // Route::get('/', 'BitcoinDashboardController@index');
-Route::get('/cryptocurrency', 'BitcoinDashboardController@index');
+Route::get('cryptocurrency', 'BitcoinDashboardController@index');
 Route::get('/', 'CryptoController@index');
 Route::get('/cards', 'CryptoController@viewCards')->name('cryptocard');
 Route::get('/loans', 'CryptoController@viewLoans')->name('cryptoloan');
